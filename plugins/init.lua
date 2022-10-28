3e2808c5208c004abba08f87abca2104c3bb5295
Luis Aldama <luisaldama@icloud.com>
2022-10-28 12:37:32 -0400

    Initial configuration
    This is the first configuration. I added a plugins folder, and completion options under `plugins/cmp.lua`

---
+++ plugins/init.lua
@@ -0,0 +1,12 @@
+vim.cmd [[
+let g:vimwiki_list = [{'path': '~/master/obsidian_research/',
+											\ 'syntax': 'markdown','ext':'.md'}]
+]]
+
+vim.cmd [[
+	let g:vimtex_view_method = 'skim'
+]]
+
+vim.cmd [[
+	syntax enable
+]]
